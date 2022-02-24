0a315484c71ff1ca9a657db4792df77834f4075c
yaya3636 <65079081+yaya3636@users.noreply.github.com>
2022-02-24 09:54:20 +0100

    Update NotifExemple.lua

--- Exemples/NotifExemple.lua
+++ Exemples/NotifExemple.lua
@@ -1,6 +1,6 @@
-Tools = dofile(global:getCurrentDirectory() .. "\\YayaTools\\Module\\Tools.lua")()
-Iphone10 = Tools.notifications({key = 0, deviceID = 0})
-Iphone12 = Tools.notifications({key = 1, deviceID = 1})
+Tools = dofile(global:getCurrentDirectory() .. "\\YayaTools\\Module\\Tools.lua")() -- Création d'une instance de Tools
+Iphone10 = Tools.notifications({key = 0, deviceID = 0}) -- Création d'une instance de Notification
+Iphone12 = Tools.notifications({key = 1, deviceID = 1}) -- Création d'une autres instance de Notification avec différent params
 
 function move()
     Iphone10:PSaferSendNotification({title = "Alerte modérateur !", msg = "Keroopin est présent sur le serveur !"})
